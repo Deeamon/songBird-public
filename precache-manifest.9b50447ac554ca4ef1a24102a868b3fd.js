2e26112b1b7b99dea9830d96b781ffb1170ba8ee
Deeamon <marinasugutova@gmail.com>
2020-02-11 00:45:08 +0300

    init: init public ver

---
+++ precache-manifest.9b50447ac554ca4ef1a24102a868b3fd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527bd9cf48a13f3aaa4d91ef38161704",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5bab81eb4945a634a27",
+    "url": "/static/css/2.b83b5d0d.chunk.css"
+  },
+  {
+    "revision": "9879517dc5e3713dfaed",
+    "url": "/static/css/main.2a9faf7a.chunk.css"
+  },
+  {
+    "revision": "e5bab81eb4945a634a27",
+    "url": "/static/js/2.d8f48b10.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d8f48b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9879517dc5e3713dfaed",
+    "url": "/static/js/main.cff6f216.chunk.js"
+  },
+  {
+    "revision": "8146f0cabe9c8f6aeb56",
+    "url": "/static/js/runtime-main.cad64008.js"
+  },
+  {
+    "revision": "89cf22f81bc1d776e20d2735d8d0cb16",
+    "url": "/static/media/sertificate.89cf22f8.pdf"
+  },
+  {
+    "revision": "169a3a76c7f8ae0911b6ea9fe098d000",
+    "url": "/static/media/success.169a3a76.wav"
+  },
+  {
+    "revision": "5acd1a089942ad6d903b600a76ef063c",
+    "url": "/static/media/wrong.5acd1a08.mp3"
+  }
+]);
